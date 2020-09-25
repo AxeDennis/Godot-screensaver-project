a6dd79558af1b644e75f6b77f05c2d32f417206f
AxeDennis <59689963+AxeDennis@users.noreply.github.com>
2020-09-25 12:16:48 +0200

    Update 2020-09-25 12:16
    The cubes now turn red if they touch eachother.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -8,15 +8,18 @@ func _ready():
 	
 func _physics_process(delta):
 	if direction == 1:
-		speed.x = 100
+		speed.x = 50
 	elif direction == 2:
-		speed.x = -100
+		speed.x = -50
 	elif direction == 3:
-		speed.y = 100
+		speed.y = 50
 	elif direction == 4:
-		speed.y = -100
+		speed.y = -50
 	
 	move_and_slide(speed)
 	
 func _on_Timer_timeout():
 	direction = floor(rand_range(1,5))
+
+func _on_Area2D_area_entered(area):
+	modulate = Color(255,0,0)
--- Node2D.gd
+++ Node2D.gd
@@ -6,13 +6,14 @@ var amount = 1
 	
 func _ready():
 	randomize()
-
+	
+func _process(delta):
 	while amount < 50:
 		var scene = load("res://Kub.tscn")
 		var cube = scene.instance()
 		location.x = rand_range(1, window_size.x)
 		location.y = rand_range(1, window_size.y)
-		amount += 1
 		add_child(cube)
 		cube.position = location
+		amount += 1
 		print("current: ", amount)
